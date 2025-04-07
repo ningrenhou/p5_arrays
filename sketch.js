9886a41b9e68bdd5e4d17410623d6676a8a6162f
Ningren-Hou <lillianforocad@gmail.com>
2025-04-06 20:14:27 -0400

    p5_arrays

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var words = [];
+var createwords = ["Zayne","🍐","❄️","黎深"];
+
+function createEmbers(x,y){
+    var originx = x;
+    var originy = y;
+}
+
+function draw () {
+    background('#0f274a');
+
+    if (frameCount % 27 === 0) {
+        var zayne = {
+            x: 0,
+            y: random (width),
+            words: random(createwords),
+            speed: random (1,3),
+            colour: color(random('#000000')),
+
+            draw: function() {
+            noStroke();
+            fill(this.colour); 
+            text(this.words,this.x,this.y); 
+            this.update(); 
+            },
+
+            update: function () {
+                this.x += this.speed;
+            }
+        };
+        words.push(zayne);
+    }
+
+    words = words.filter(function(zayne) {
+        return zayne.x < height;
+    });
+
+    for (var i = 0; i < words.length; i +=1 ) {
+            words[i].draw();
+    }
+}
+
+function setup () {
+    createCanvas(windowWidth, windowHeight);
+    textAlign(CENTER, CENTER);  
+    textSize(20);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+
+// References
+// for: https://p5js.org/reference/p5/for/
+// []: https://www.w3schools.com/jsref/jsref_array[].asp
+// random: https://p5js.org/reference/p5/random/
+// width: https://p5js.org/reference/p5/width/
